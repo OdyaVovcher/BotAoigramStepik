b6360a2b4a2d4ced9d6610b8f09bd3b7fc17e957
V.C <bloodoof1234@gmial.com>
2023-02-16 17:50:53 +0600

    request testing :wq

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("Hello world")
-print("for new commit")
+import requests
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
+
