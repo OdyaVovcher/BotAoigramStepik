ab6209dbe0e4ce25c402f05bb188fd98b10d64a4
V.C <bloodoof1234@gmial.com>
2023-02-17 14:47:59 +0600

    message to user

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
 import requests
+import time
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6245446770:AAFJ2ULztGn22ur_tFE4I2Lc-jj2xDoQ5tk'
+TEXT: str = 'Ура! Классный апдейт'
+MAX_COUNTER: int = 20
 
-response = requests.get(api_url)
+offset: int = -2
+counter: int = 0
+chat_id: int
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
-    print(2132314)
 
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
