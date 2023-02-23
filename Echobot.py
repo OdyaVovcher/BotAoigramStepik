21fe136da7cdd35eb5f3d407da922cc3db3577ab
V.C <bloodoof1234@gmial.com>
2023-02-23 14:00:54 +0600

    Final EchoBot

--- Echobot.py
+++ Echobot.py
@@ -9,24 +9,25 @@ API_TOKEN: str = '6245446770:AAFJ2ULztGn22ur_tFE4I2Lc-jj2xDoQ5tk'
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
 
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь в ответ я пришлю тебе твое сообщение')
 
 
-@dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
-    
-
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
