d99e2f8a80798dc02c74e92d4504664d9605bd05
Odya_Vovcher <bloodoof1234@gmail.com>
2023-02-25 22:53:23 +0600

    Main settings and constants

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,2 +1,21 @@
 #25.02.2023
 #GuessNumber Bot
+import random
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Text, Command
+
+BOT_TOKEN: str = '6245446770:AAFJ2ULztGn22ur_tFE4I2Lc-jj2xDoQ5tk'
+
+bot: Bot = Bot(BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+ATTEMPTS: int = 5
+
+user: dict = {
+    'in_game': False,
+    'secret_number': None,
+    'attemps': None,
+    'total_games': 0,
+    'wins':0
+}
